2fb720c48ef8497a8094691f807edd5fb0732373
CitizenOfZion <34536872+CitizenOfZion@users.noreply.github.com>
2017-12-14 14:25:43 +0530

    Initial Upload using Chrome

---
+++ makefile
@@ -0,0 +1,22 @@
+SRCPATH	= ./src
+OBJPATH = ./obj
+
+SRCS	= $(wildcard $(SRCPATH)/*.c)
+OBJS	= ${SRCS:.c=.o}
+
+
+.c.o:
+	@echo "Compiling" $< "..."
+	cc -c $<
+
+all: app putobj
+
+app:  $(OBJS)
+
+
+
+putobj:
+	(mv *.o $(OBJPATH);cd $(OBJPATH);pwd;sudo chmod 777 *.o)
+
+cleanall:
+	-rm -rf $(OBJPATH)/*.o
---
+++ src/pipe.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(void)
+{
+        int     fd[2], nbytes;
+        pid_t   childpid;
+        char    string[] = "Hello, world!\n";
+        char    readbuffer[80];
+
+        pipe(fd);
+        
+        if((childpid = fork()) == -1)
+        {
+                perror("fork");
+                exit(1);
+        }
+
+        if(childpid == 0)
+        {
+                /* Child process closes up input side of pipe */
+                close(fd[0]);
+
+                /* Send "string" through the output side of pipe */
+                printf("Sent string before: %s", string);
+                write(fd[1], string, (strlen(string)+1));
+                exit(0);
+        }
+        else
+        {
+                /* Parent process closes up output side of pipe */
+                close(fd[1]);
+
+                /* Read in a string from the pipe */
+                nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
+                printf("Received string: %s", readbuffer);
+        }
+	for(;;); //To test if both processes (parent and child) are running
+        return(0);
+}
